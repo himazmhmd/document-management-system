e1b868697e382ff748fa45f58bf1346ecf5ef6fa
nerodroid <nerodroid.dev@gmail.com>
2021-05-17 01:24:58 +0530

    login register working

--- server/routes/User.js
+++ server/routes/User.js
@@ -9,6 +9,7 @@ import * as controller from "../controller/User.js";
 const router = express.Router();
 
 router.post("/sign-up", controller.createUser);
+router.post("/login",controller.loginUser); 
 router.get("/", controller.getUsers);
 router.get("/:id", controller.getUser);
 router.delete("/:id", controller.deleteUser);
